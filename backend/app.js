d745ae8ab78022125b9085d6b75270e0d4f10fe0
Riddler <2584275@gmail.com>
2021-08-13 10:36:32 +0300

    pr15.4

--- backend/app.js
+++ backend/app.js
@@ -4,36 +4,36 @@ const mongoose = require('mongoose');
 const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
 const crypto = require('crypto');
-const cors = require('cors');
+// const cors = require('cors');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000'
-];
+// const allowedCors = [
+//  'https://praktikum.tk',
+//  'http://praktikum.tk',
+//  'localhost:3000'
+// ];
 
-app.use(function(req, res, next) {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  next();
-});
-const requestHeaders = req.headers['access-control-request-headers'];
-const { method } = req;
-// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-// Если это предварительный запрос, добавляем нужные заголовки
-if (method === 'OPTIONS') {
+// app.use(function(req, res, next) {
+  //const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+ // // проверяем, что источник запроса есть среди разрешённых
+ // if (allowedCors.includes(origin)) {
+ //   res.header('Access-Control-Allow-Origin', origin);
+ // }
+ // next();
+// });
+// const requestHeaders = req.headers['access-control-request-headers'];
+//const { method } = req;
+// // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// // Если это предварительный запрос, добавляем нужные заголовки
+// if (method === 'OPTIONS') {
 // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-res.header('Access-Control-Allow-Headers', requestHeaders);
-return res.end();
-}
+// res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+// res.header('Access-Control-Allow-Headers', requestHeaders);
+// return res.end();
+// }
 
 const randomString = crypto
   .randomBytes(16) // сгенерируем случайную последовательность 16 байт (128 бит)
@@ -53,9 +53,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.use(cors({
-  origin: allowedCors,
-}));
+//app.use(cors({
+//  origin: allowedCors,
+//}));
 
 app.use(requestLogger); // подключаем логгер запросов
 
